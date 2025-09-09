ed7c326c62bcacbfa797e3adcff364edc5df9ff6
Iva Conte <ivconte@gmail.com>
2025-09-09 13:26:24 -0300

    fix: api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://mais-bolo.onrender.com/mais-bolo-backend/api' // Update this with your production API URL
+  apiUrl: 'https://mais-bolo-backend.onrender.com/api'
 };
