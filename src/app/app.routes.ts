9a4111d7c980e95fb8e903f7a3efd059e2fc2d36
Iva Conte <ivconte@gmail.com>
2025-09-10 09:55:27 -0300

    feat: login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
 import { Pedido }  from './components/pedido/pedido';
+import { Login } from './components/login/login';
 
 
 export const routes: Routes = [
+    {
+        path: '', component: Login
+    },
+    {
+        path: 'login', component: Login
+    },
     {
         path: 'pedido', component: Pedido
     }
