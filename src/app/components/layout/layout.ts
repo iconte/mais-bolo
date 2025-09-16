6bd01f51059039d46e11375f70ce0ebd5d5b100a
Iva Conte <ivconte@gmail.com>
2025-09-16 20:24:52 -0300

    feat: menu lateral

--- src/app/components/layout/layout.ts
+++ src/app/components/layout/layout.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
+import { NgClass, CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgClass, CommonModule],
   templateUrl: './layout.html',
   styleUrls: ['./layout.css']
 })
 export class LayoutComponent {
+  menuExpandido: boolean = false;
   constructor(private router: Router) {}
 
   navigateTo(path: string) {
